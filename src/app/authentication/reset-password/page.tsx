a269b9929079b9f77fc00e4e3ab7adf5ef4c461a
David Adekunle <doyin9210@gmail.com>
2025-07-07 14:53:55 +0100

    added suspense to reset-password page

--- src/app/authentication/reset-password/page.tsx
+++ src/app/authentication/reset-password/page.tsx
@@ -1,5 +1,6 @@
 import { ResetPassword } from "@/components/auth/ResetPassowrd";
 import { getLayoutData } from "@/lib/strapi/data-loader";
+import { Suspense } from "react";
 
 export default async function ResetPasswordPage() {
   const { translations } = await getLayoutData({ cached: true });
@@ -7,7 +8,9 @@ export default async function ResetPasswordPage() {
   return (
     <div className="flex min-h-full items-center justify-center auth-bg px-4 py-12 sm:px-6 lg:px-8">
       <div className="auth-form-container p-10 rounded-xl w-full max-w-[440px] space-y-6">
-        <ResetPassword translations={translations ?? {}} />
+        <Suspense fallback={<div>Loading...</div>}>
+          <ResetPassword translations={translations ?? {}} />
+        </Suspense>
       </div>
     </div>
   );
